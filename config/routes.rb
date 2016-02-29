661712b7f4795a39390226a05c2b6fe271880ea5
Eduardo Silva <eduardomail@gmail.com>
2016-02-29 18:42:34 -0300

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,7 @@ Rails.application.routes.draw do
     resources :users
   end
   resource :confirmation, only: [:show]
-
-  get '/:locale' => 'home#index', locale: /en|pt/
-
-
+  resource :user_sessions, only: [:create, :new, :destroy]
   root 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
